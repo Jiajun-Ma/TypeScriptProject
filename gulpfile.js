ef40630e791588e26a0e534aa6631d35c38ce44e
Jiajun-Ma <175010774@qq.com>
2019-02-05 22:52:44 +0800

    增加了自动构建

--- gulpfile.js
+++ gulpfile.js
@@ -7,35 +7,59 @@ var gutil = require("gulp-util");
 var uglify = require('gulp-uglify');
 var sourcemaps = require('gulp-sourcemaps');
 var buffer = require('vinyl-buffer');
+var sass = require('gulp-sass')
+var browserSync = require('browser-sync').create()
 
-// 设置保存自动编译
-var watchedBrowserify = watchify(browserify({
-  basedir: '.',
-  debug: true,
-  entries: ['src/main.ts'],
-  cache: {},
-  packageCache: {}
-}).plugin(tsify));
-watchedBrowserify.on("update", bundle, copyHtml);
-watchedBrowserify.on("log", gutil.log);
 
-// 设置gulp任务
-gulp.task("copy-html", copyHtml);
-gulp.task("default", ["copy-html"], bundle);
 var paths = {
-  pages: ['src/*.html']
+  source: {
+    pages: './src/index.html',
+    sass: './src/scss/**/*.scss',
+    typescript: './src/ts/main.ts'
+  },
+  target: {
+    pages: './dist',
+    css: './dist/css',
+    javascript: './dist/js'
+  }
 };
-function copyHtml() {
-  return gulp.src(paths.pages)
-    .pipe(gulp.dest("dist"));
-}
-function bundle() {
-  return watchedBrowserify
+
+gulp.task('default', ['serve'], function () { })
+
+// 静态服务器 + 监听 scss/html 文件
+gulp.task('serve', ['sass', 'copy-html', 'compile-typeScript'], function () {
+  browserSync.init({
+    server: paths.target.pages
+  });
+  gulp.watch(paths.source.sass, ['sass']).on('change', browserSync.reload);
+  gulp.watch("./src/ts/**/*.ts", ['compile-typeScript']).on("change", browserSync.reload)
+  gulp.watch("./src/**/*.html", ['copy-html']).on('change', browserSync.reload);
+});
+
+gulp.task("copy-html", function () {
+  return gulp.src(paths.source.pages)
+    .pipe(gulp.dest(paths.target.pages));
+});
+
+gulp.task('sass', function () {
+  return gulp.src(paths.source.sass)
+    .pipe(sass({ sourcemap: true }))
+    .pipe(gulp.dest(paths.target.css))// Write the CSS & Source maps
+    // .pipe(filter('**/*.css')) // Filtering stream to only css files
+    .pipe(browserSync.reload({ stream: true }));
+});
+
+gulp.task('compile-typeScript', function () {
+  return browserify({
+    basedir: '.',
+    debug: true,
+    entries: [paths.source.typescript],
+    cache: {},
+    packageCache: {}
+  })
+    .plugin(tsify)
     .bundle()
     .pipe(source('bundle.js'))
-    .pipe(buffer())
-    .pipe(sourcemaps.init({ loadMaps: true }))
-    .pipe(uglify())
-    .pipe(sourcemaps.write('./'))
-    .pipe(gulp.dest("dist"));
-}
+    .pipe(gulp.dest(paths.target.javascript));
+})
+
--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,6 +1,6 @@
 import { sayHello } from "./greet"
 
-const name = 'TypeScript!!!'
+const name = 'TypeScript'
 
 function showHello(divId: string, name: string) {
   const elt = document.getElementById(divId)
@@ -11,4 +11,3 @@ window.onload = function () {
   showHello('greeting', name)
 }
 
-console.log(sayHello(name))
